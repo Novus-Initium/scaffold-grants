521ce1a8b91bd191f4d89268c13f3719be80ee37
DavNej <david.nejar@gmail.com>
2024-07-13 17:18:38 +0200

    [UI] base edition

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Scaffold-Grant-Stack",
+  description: "Built with Scaffold-Grants-Stack",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
