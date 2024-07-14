02484e3df1261b5562b7d64937b79ae59221c3cc
Jade Clyne <krclyne@gmail.com>
2024-07-14 04:55:52 +0200

    Adding create grant round page

---
+++ packages/nextjs/app/grants/create/page.tsx
@@ -0,0 +1,17 @@
+import CreateGrantRound from "~~/components/CreateGrantRound";
+
+export default function NewProjectPage() {
+  return (
+    // <main className="p-10">
+    //   <div className="card bg-base-100 shadow-lg">
+    //     <div className="card-body">
+    //       <h2 className="card-title">New Grant Round</h2>
+
+    //       <div className="card-actions flex flex-col">
+    <CreateGrantRound />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
+  );
+}
